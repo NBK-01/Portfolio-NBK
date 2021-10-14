bcd91946c69a002ad69c5c4e51e85c2817cc927d
unknown <nayefkanaan03@gmail.com>
2021-10-14 14:17:07 +0300

    Adding contact js and css files

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,18 +1,9 @@
-var loader = document.getElementById("preloader");
-
-window.addEventListener("load", function(){
-    loader.style.display = "none";
-})
-
 const toggleBtn = document.querySelector('.toggle-btn');
 const ul = document.querySelector('nav .link');
-const header = document.querySelector('#vanta');
+const header = document.querySelector('#main');
 
 toggleBtn.addEventListener('click', () => {
     toggleBtn.classList.toggle('active');
     ul.classList.toggle('active');
     header.classList.toggle('skew');
 })
-
-
-
