3f370fd6fd12b4ef69b418972c4c9fb31f257ab2
unknown <nayefkanaan03@gmail.com>
2021-10-14 14:18:00 +0300

    WIP

--- assets/js/contact.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.querySelector('.toggle-btn');
-const ul = document.querySelector('nav .link');
-const header = document.querySelector('#main');
-
-toggleBtn.addEventListener('click', () => {
-    toggleBtn.classList.toggle('active');
-    ul.classList.toggle('active');
-    header.classList.toggle('skew');
-})
