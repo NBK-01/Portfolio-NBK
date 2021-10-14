bdf3fddbbffa80fce0acbfa1eb0352d10a001f3a
unknown <nayefkanaan03@gmail.com>
2021-10-14 12:39:32 +0300

    init navigation anim

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,13 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const ul = document.querySelector('nav .link');
+const header = document.querySelector('#vanta');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    ul.classList.toggle('active');
+    header.classList.toggle('skew');
+})
