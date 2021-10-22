3baa0416fb339b61964521c2447c8c025a5b81a1
Nayef Kanaan <nayefkanaan03@gmail.com>
2021-10-22 22:06:50 +0400

    Deployment debug

---
+++ dist/index.6a69a116.js
@@ -0,0 +1,23 @@
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function() {
+    loader.style.display = "none";
+});
+const toggleBtn = document.querySelector('.toggle-btn');
+const ul = document.querySelector('nav .link');
+const header = document.querySelector('#vanta');
+const workBtn = document.querySelector('.work-link');
+toggleBtn.addEventListener('click', ()=>{
+    toggleBtn.classList.toggle('active');
+    ul.classList.toggle('active');
+    header.classList.toggle('skew');
+});
+header.addEventListener('click', ()=>{
+    toggleBtn.classList.remove('active');
+    ul.classList.remove('active');
+    header.classList.remove('skew');
+});
+workBtn.addEventListener('click', ()=>{
+    window.location.href = 'work.html';
+});
+
+//# sourceMappingURL=index.6a69a116.js.map
