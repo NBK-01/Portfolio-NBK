0b3e8d6981a9e4cbfdd7320e723607077471ee4e
Nayef Kanaan <nayefkanaan03@gmail.com>
2021-10-25 15:18:38 +0400

    Optimization

---
+++ dist/work.1e6d1e2b.js
@@ -0,0 +1,23 @@
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function() {
+    loader.style.display = "none";
+});
+const toggleBtn = document.querySelector('.toggle-btn');
+const ul = document.querySelector('nav .link');
+const header = document.querySelector('#work');
+toggleBtn.addEventListener('click', ()=>{
+    toggleBtn.classList.toggle('active');
+    ul.classList.toggle('active');
+    header.classList.toggle('skew');
+});
+header.addEventListener('click', ()=>{
+    toggleBtn.classList.remove('active');
+    ul.classList.remove('active');
+    header.classList.remove('skew');
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
+//# sourceMappingURL=work.1e6d1e2b.js.map
